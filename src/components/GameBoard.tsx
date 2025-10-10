d220b1e24849c722c9e8c2dd05103424829c6031
Niccolò Baldassarri <Baldarello@users.noreply.github.com>
2025-10-10 15:03:42 +0200

    feat: Display turn status message
    Adds a dedicated area to display game turn status messages to the user.
    Also adjusts some styling for player mats and refactors the "last trick" button to include an icon for better visual feedback.

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -383,6 +383,12 @@ export const GameBoard = observer(() => {
                 </div>
             </div>
             
+            <div className="turn-status-container">
+                <div className="turn-message">
+                    {message}
+                </div>
+            </div>
+
             <div className="bottom-bar">
                 <div className="player-info-group">
                     <div className="human-score-container" ref={playerMenuRef}>
@@ -425,14 +431,12 @@ export const GameBoard = observer(() => {
                             title={historyButtonTitle}
                             disabled={!lastTrick}
                         >
-                            <span>{TH.lastTrick}</span>
+                            <span className="last-trick-text">{TH.lastTrick}</span>
+                            <svg className="last-trick-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.25 2.52.77-1.28-3.52-2.09V8z"/></svg>
                             <CardView card={lastTrick.humanCard} lang={language} cardDeckStyle={cardDeckStyle} />
                             <CardView card={lastTrick.aiCard} lang={language} cardDeckStyle={cardDeckStyle} />
                         </button>
                     )}
-                </div>
-                <div className="turn-message">
-                    {message}
                 </div>
             </div>
              {cardForElementalChoice && (
