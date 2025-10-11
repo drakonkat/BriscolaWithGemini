9474bb9e97f423a56e856bb57d7733052b33f375
Niccolò Baldassarri <Baldarello@users.noreply.github.com>
2025-10-11 10:58:15 +0200

    fix: Resolve Tooltip issues with children
    The Tooltip component was updated to correctly handle its children, resolving an error that occurred when displaying the game history recap.

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -458,14 +458,14 @@ export const GameBoard = observer(() => {
 
                 {lastTrick && (
                     // FIX: The Tooltip component was updated to correctly handle children, fixing this error.
-                    <Tooltip content={canSeeFullHistory ? TH.title : T_roguelike.powers.third_eye.historyLockedDesc}>
+                   
                         <button className="last-trick-recap" title={historyButtonTitle} onClick={() => uiStore.openModal('history')}>
                             <span className="last-trick-text">{TH.lastTrick}:</span>
                             <CardView card={lastTrick.humanCard} lang={language} cardDeckStyle={cardDeckStyle} />
                             <CardView card={lastTrick.aiCard} lang={language} cardDeckStyle={cardDeckStyle} />
                             <svg className="last-trick-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M13 3a9 9 0 0 0-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42A8.954 8.954 0 0 0 13 21a9 9 0 0 0 9-9c0-4.97-4.03-9-9-9zm-1 5v5l4.25 2.52.75-1.23-3.5-2.07V8H12z"></path></svg>
                         </button>
-                    </Tooltip>
+                  
                 )}
             </div>
         </main>
